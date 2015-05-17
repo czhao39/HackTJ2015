203366e313eea3c9158e34dae68fa2dba1d69875
Grey Golla <bobobalink@gmail.com>
2015-05-17 00:38:33 -0400

    made first half of metaMinds thingy

--- metaMinds.py
+++ metaMinds.py
@@ -1,3 +1,17 @@
-from flask import jsonify
+import simplejson
+import requests
+from random import randint
+
 def sentiment(texts):
-    text = [{"label": None, "value": x} for x in texts]
+    thing = simplejson.dumps([{"label": None, "value": x} for x in texts])
+    session = requests.Session()
+    session.headers.clear()
+    session.headers["Authentication"] = "Basic zzjfd812EiDqxeuqC91BnwaOk5YHWsh9FNUqkKnS8JFcwe8PHw"
+    dataset = requests.post('https://www.metamind.io/api/v1.2/datasets', data={'name': 'temp' + randint(0, 10000000000).__str__(), 'private': False, 'data_type': 'text'})
+    decoder = simplejson.JSONDecoder()
+    id = decoder.decode(dataset.content)["id"]
+    url = 'https://www.metamind.io/api/v1.2/datasets/' + id.__str__() + '/entries'
+    print session.post(url, data={'entry_type': 'text', 'skip_invalid_entries': True, 'entries': thing}).content
+    print session.get(url).content
+
+sentiment(['abc', 'def', 'ghi'])
