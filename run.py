9d68e3f420d9c81c17f45c9fc2184947b58c99ab
Grey Golla <bobobalink@gmail.com>
2015-05-16 21:56:07 -0400

    changed things

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
 from flask import Flask, request, jsonify
+from twitter import *
+
 app = Flask(__name__, static_url_path='')
 
 # GET request this for the national data and stuff
 # return json if possible
 
-STATES = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']
+STATES = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
+
 
 @app.route('/nation')
 def getNation():
@@ -15,6 +18,7 @@ def getNation():
         db[i] = '#ff0000'
     return jsonify(**db)
 
+
 # GET request for when someone clicks on a state
 # example: /state?s=va
 @app.route('/state')
@@ -22,9 +26,11 @@ def getState():
     state = request.args.get('s')
     return False
 
+
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
+
 
 import sys
 
