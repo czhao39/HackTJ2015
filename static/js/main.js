d90f05e99b4475bcc06b18b3b41ac77d9435c797
Grey Golla <bobobalink@gmail.com>
2015-05-17 13:03:35 -0400

    fixed opacity properly

--- static/js/main.js
+++ static/js/main.js
@@ -158,7 +158,7 @@ $(document).ready(function() {
                     neg = pos
                     pos = tmp
                 }
-                filstr = "rgba(" + Math.round(neg/total*255) + ",0," + Math.round(pos/total*255) + "," + Math.round(Math.min(255, 255*complete/opacity_threshold)) + ")";
+                filstr = "rgba(" + Math.round(neg/total*255) + ",0," + Math.round(pos/total*255) + "," + Math.round(Math.min(255, 255*opacity_threshold/complete)) + ")";
                 if (key == "MI") {
                     key = "MI-";
                     data["MI-"] = data["MI"];
