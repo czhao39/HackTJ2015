be04462c902ee5e868aa620082c96c3556ab4871
Haicheng Charles Zhao <czhao39@gmail.com>
2015-05-17 01:02:59 -0400

    twitter api

--- twitterStuff.py
+++ twitterStuff.py
@@ -1,10 +1,12 @@
 import twitter
 from greysFancyTwitterGarbage import *
+import requests
 
 import simplejson
 
 api = twitter.Api(consumer_key="kVPvvr7To3gmFhi1hqHiOMTOk", consumer_secret="ThxenYosQipFLuievc4rZcFcFzDu5b2xe5utOParzhh4YJoHgh", access_token_key="2924997039-Cygnh0IdQ2p1A8tLy9ulzLo4ShZf9slswefWPhD", access_token_secret="wcfN2N90oUVjmVEXfdIx4FeImO2A1GEWwv63GOg4w0Ccl")
 
+r = requests.get("https://maps.googleapis.com/maps/api/geocode/json", params={"latlng": "", "key": "AIzaSyCPk9E5t02n-hQHfRqOLZFGFAcYbPCtTVk"})
 jsonString = open("static/data/keywords.txt").read()
 jsonDict = simplejson.JSONDecoder().decode(jsonString)
 masterDict = {}
